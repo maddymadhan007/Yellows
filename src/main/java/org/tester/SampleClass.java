0f31285ebf82a5c343d8b603f1e0692eb0680fac
maddymadhan007 <madhang31991@gmail.com>
2023-01-22 17:19:50 +0530

    seconds

---
+++ src/main/java/org/tester/SampleClass.java
@@ -0,0 +1,17 @@
+package org.tester;
+
+public class SampleClass {
+	private void madhan() {
+		System.out.println("Handsome");
+	}
+	private void madhan1() {
+		System.out.println("Handsome1");
+	}
+	private void madhan2() {
+		System.out.println("Handsome2");
+	}
+	private void madhan3() {
+		System.out.println("Handsome3");
+	}
+
+}
